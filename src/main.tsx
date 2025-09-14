5c2e4a574906ff28f9b6f4d4d395e6c1d59249ce
鲨鱼辣椒 <monishine_code@163.com>
2025-09-14 22:53:30 +0800

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,33 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.tsx'
 import './index.css'
 
+const obj = {
+  name: 'duyi',
+  age: 18,
+}
+
+const arr = [1, 2, 3, 4, 5]
+
+let a = '1'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
     <h1>Hello Vite + React + UnoCSS</h1>
+    <p title={a}>{arr}</p>
   </StrictMode>,
 )
+
+setTimeout(() => {
+  a = '2'
+}, 1000)
+
+const h1 = (
+  <>
+    <h1>Hello Vite + React + UnoCSS</h1>
+  </>
+)
+
+const p = <h1>Hello Vite + React + UnoCSS</h1>
+console.log(h1)
+console.log(p)
